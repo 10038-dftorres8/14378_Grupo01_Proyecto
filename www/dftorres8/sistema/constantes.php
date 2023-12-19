ea2e3feb3f15d08293de84574e248f82a19caf01
10038_dftorres8 <dftorres8@espe.edu.ec>
2023-12-18 20:04:58 -0500

    delet

--- www/dftorres8/sistema/constantes.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define("SERVER","db");
-  define("USER","root");
-  define("PASS","test");
-  define("BD","matriculacionfinal");
-  define("PATH","../../imagenes/autos/");
-  define("PATH2","../../imagenes/sellos/");
- ?>
